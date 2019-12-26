f4e39c490ffb85189bda86d803a3864160200e23
Maxime Monge <maxime.monge@yahoo.fr>
2019-12-26 16:07:39 +0100

    ajout google_map_api.xml et sa clé api + modification permissions + envoi des coordonnées géo actuelles par sms

--- app/src/main/java/com/example/projet_balat_monge/MainActivity.java
+++ app/src/main/java/com/example/projet_balat_monge/MainActivity.java
@@ -1,37 +1,50 @@
 package com.example.projet_balat_monge;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 public class MainActivity extends AppCompatActivity {
+    private GoogleMap map;
+    private FusedLocationProviderClient fusedLocationClient;
+    private Double latitude = 0.0;
+    private Double longitude = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        verifierPermission();
+        verifierPermissionSms();
+        verifierPermissionInternet();
+        verifierPermissionAccessFineLocation();
+        initialiserLocalisation();
     }
 
     public void sendSms(View view) {
         EditText numeroText = (EditText) findViewById(R.id.editTextNumero);
-        String message = "toto";
+
+        String message = "https://www.google.com/maps/place/" + latitude.toString() + "," + longitude.toString();
         String numero = numeroText.getText().toString();
 
         String[] parts = numero.split(";");
 
         Context context = getApplicationContext();
         int duration = Toast.LENGTH_SHORT;
-        if (!message.isEmpty()) {
             try {
                 SmsManager smgr = SmsManager.getDefault();
                 for (String numeroTel : parts) {
@@ -40,16 +53,12 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(context, "Message envoyé", duration).show();
             }
             catch (Exception e){
-                int checkPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS);
+                ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS);
                 Toast.makeText(context, "Échec de l'envoi", duration).show();
             }
-        }
-        else {
-            Toast.makeText(context, "Erreur : numéro incorrect ou message vide", duration).show();
-        }
     }
 
-    private void verifierPermission() {
+    private void verifierPermissionSms() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
             if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SEND_SMS)) {
                 // Ne rien faire
@@ -59,5 +68,47 @@ public class MainActivity extends AppCompatActivity {
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, 1);
             }
         }
+    }
+
+    private void verifierPermissionInternet() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.INTERNET)) {
+                // Ne rien faire
+            }
+
+            else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, 1);
+            }
+        }
+    }
+
+    private void verifierPermissionAccessFineLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                map.setMyLocationEnabled(true);
+                map.getUiSettings().setMyLocationButtonEnabled(true);
+
+            }
+
+            else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                map.setMyLocationEnabled(false);
+                map.getUiSettings().setMyLocationButtonEnabled(false);
+            }
+        }
+    }
+
+    private void initialiserLocalisation() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    latitude = location.getLatitude();
+                    longitude = location.getLongitude();
+                }
+            }
+        });
     }
 }
